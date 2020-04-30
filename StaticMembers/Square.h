429c4eb95e8eca38f5b26b67858fb42002f04e1d
zhuzhu <zhuzhu18@aliyun.com>
2020-04-30 14:56:32 +0800

    Create Square.h

---
+++ StaticMembers/Square.h
@@ -0,0 +1,15 @@
+#pragma once
+class Square {
+private:
+	double side;
+	static int numberOfObjects;
+public:
+	double getSide() { return side; }
+	void setSide(double side) { this->side = side; }
+	Square(double side) { this->side = side; 
+	numberOfObjects++;
+	}
+	Square() :Square(1.0) {}
+	double getArea() { return side * side; }
+	static int getNumberOfObjects() { return numberOfObjects; }
+};
