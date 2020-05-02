9f2ece08e409b4c537b9251a5feeb967c48801dd
zhuzhu <zhuzhu18@aliyun.com>
2020-05-02 22:07:53 +0800

    Create Rectangle.h

---
+++ 继承/Rectangle.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Shape.h"
+class Rectangle:public Shape {
+private:
+	double width, height;
+public:
+	double getWidth() const;
+	void setWidth(double w);
+	double getHeight() const;
+	void setHeight(double h);
+	Rectangle(double w, double h, Color c, bool fill);
+	double getArea() const;
+};
