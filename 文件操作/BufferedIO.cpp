2417d8d5c0a3224b51604745a4233752c17273b0
zhuzhu <zhuzhu18@aliyun.com>
2020-06-08 09:27:13 +0800

    输出缓冲区字符的ASCII码

--- 文件操作/BufferedIO.cpp
+++ 文件操作/BufferedIO.cpp
@@ -5,7 +5,7 @@ int main() {
 	ofstream output{ "scores.txt" };
 	output << "zhuzhihua  " << 98 << endl;
 	output << "liwenjuan  " << 100 << endl;
-	cout << "文件的大小：" << fs::file_size(p) << endl;*/
+	cout << "文件的大小：" << fs::file_size(p) << endl;
 	auto* p = cin.rdbuf();
 	cout << cin.peek() << endl;
 	static int i{ 0 };
